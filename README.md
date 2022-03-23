12cfc2589dab98cdb26bb112d85df406eee282ce
Nelli Wulandari <nelliwlnd30@gmail.com>
2022-03-23 08:35:59 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Hasil Screnshoot tugas_praktikum jobsheet 2 Grid
+# Hasil Screnshoot tugas_praktikum jobsheet 2 grid
 ![Screenshot Dashboard Oracle](images/grid.jpeg)
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:jobsheet2/build_in_widget/test_widget.dart';
+import 'package:jobsheet2/build_in_layout_widget/grid.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GridViewSample());
 } 
